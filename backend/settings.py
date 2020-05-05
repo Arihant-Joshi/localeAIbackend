c233d64e3b30489167d7fe224620383501659fa2
Arihant Joshi <joshi11.arihant@gmail.com>
2020-05-05 23:07:21 +0530

    ADDED Writeup

--- backend/settings.py
+++ backend/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fqksvcvf',
-        'USER': 'fqksvcvf',
-        'PASSWORD': 'qlKqs5DhvDtVAzs7KfrY85tqfjJrY_1N',
+        'NAME': os.environ.get("DB_USER"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
         'HOST': 'drona.db.elephantsql.com',
         'PORT': '5432'
     }
